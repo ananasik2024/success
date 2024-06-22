b71e6759ac84a7e735b0796be81a2df651286669
ANASTASIYA <shagrai_anastasiya13@mail.ru>
2024-06-22 17:09:53 +0300

    added lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 1;
+int b = 18;
+int c = 25;
+int d = 3;
+int e = 8;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
